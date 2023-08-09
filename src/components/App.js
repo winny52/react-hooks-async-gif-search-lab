8862bb8377331cf6542737cdad791f68534b6ba1
WinnySigilai <winnysigilai02@gmail.com>
2023-08-09 13:54:06 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-
+import GifListContainer from "../containers/GifListContainer";
 import NavBar from "./NavBar";
 
-// the App component should render out the GifListContainer component
 
 function App() {
   return (
     <div>
       <NavBar color="black" title="Giphy Search" />
+      <GifListContainer />
     </div>
   );
 }
